189f7daf0ea7695d92f20c570edc410f4881704c
invistoy <invixo93@gmail.com>
2025-02-25 21:35:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function toggleDropdown(id) {
+    let element = document.getElementById(id);
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
+function toggleDropdownTwo(id) {
+    let element = document.getElementById(id);
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
